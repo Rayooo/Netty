ab9d0f7864a1d556545b84a101342fdd7209d26b
Ray <chenxiaolei.ray@gmail.com>
2017-05-20 11:35:22 +0800

    netty的hello world

---
+++ Echo/src/main/java/EchoClientHandler.java
@@ -0,0 +1,31 @@
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.util.CharsetUtil;
+
+/**
+ * Created by Ray on 2017/5/20.
+ */
+@ChannelHandler.Sharable
+public class EchoClientHandler extends SimpleChannelInboundHandler<ByteBuf> {
+
+    //在到服务器的连接已经建立之后将被调用
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {     //当被通知Channel是活跃的时候，发送一条消息
+        ctx.writeAndFlush(Unpooled.copiedBuffer("Netty rocks!", CharsetUtil.UTF_8));
+    }
+
+    //当从服务器接收到一条消息时被调用
+    protected void channelRead0(ChannelHandlerContext ctx, ByteBuf msg) throws Exception {  //记录已接受，消息的转储
+        System.out.println("Client received: " + msg.toString(CharsetUtil.UTF_8));
+    }
+
+    //在处理过程中引发异常时被调用
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
